4fb5bc4b128a851bd6adb26515e53e4e77c0bc56
PHR1990 <pedro.ruschel@hotmail.com>
2021-01-23 18:41:21 +0100

    - Updated wiremocks  - Added bulma css  - Added more types and overall data  - Messed a little bit more with home page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {InvestmentsService} from "../service/investments.service";
-import {InvestmentSummary} from "../service/investments.interface";
+import {InvestmentSummary, RiskType, Type} from "../service/investments.interface";
+import {ThemePalette} from "@angular/material/core";
 
 
 @Component({
@@ -12,15 +13,40 @@ export class HomeComponent implements OnInit {
 
   constructor(private investmentsService: InvestmentsService) { }
 
-  displayedColumns: string[] = ['name', 'invested', 'income'];
+  displayedColumns: string[] = ['name', 'invested', 'income', 'type', 'liquidity', 'risk', 'averageInterestRate'];
+  grandTotal: number = 0;
+  investedTotalAmount: number =0;
 
-
-  investmentsSummary:any =  {} as InvestmentSummary[];
+  investmentsSummary: InvestmentSummary[] = [];
 
   ngOnInit(): void {
     this.investmentsService.getInvestments().subscribe(resp => {
       this.investmentsSummary = resp.investmentsSummary;
+
+      this.investmentsSummary.forEach(investmentSummary => {
+        if (investmentSummary.type !== Type.SavingsAccount) {
+          this.investedTotalAmount+=investmentSummary.invested + investmentSummary.totalIncome;
+        }
+        this.grandTotal+=investmentSummary.invested + investmentSummary.totalIncome;
+
+      })
     });
+  }
+
+  getChipColor(risk: RiskType) : ThemePalette {
+
+    if (risk === RiskType.High) {
+      console.log(risk);
+      return 'warn'
+    } else if (risk === RiskType.Medium) {
+      console.log(risk);
+      return 'accent'
+    } else if (risk === RiskType.Low) {
+      console.log(risk);
+      return 'primary'
+    }
+    return 'primary';
+
   }
 
 }
--- frontend/src/app/service/investments.interface.ts
+++ frontend/src/app/service/investments.interface.ts
@@ -2,8 +2,31 @@ export interface InvestmentSummary {
 
   "name" : string,
   "invested" : number,
-  "totalIncome" : number
+  "totalIncome" : number,
+  "type" : Type,
+  "liquidity" : LiquidityType,
+  "risk" : RiskType,
+  "averagePercentage" : number | string
 
+}
+
+export enum RiskType {
+  Minimum = "Minimum",
+  Low = "Low",
+  Medium = "Medium",
+  High = "High",
+}
+
+export enum LiquidityType {
+  Instant = "Instant",
+  Fast = "Fast",
+  Variable = "Variable",
+}
+
+export enum Type {
+  SavingsAccount = "Savings Account",
+  Stocks = "Stocks",
+  Loans = "Loans"
 }
 
 export interface InvestmentsSummaryResponse {
